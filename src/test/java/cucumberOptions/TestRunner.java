7f222da186182816040766ffb6344fdb18d0d446
Héctor Gerardo Silva Ayala <39529919+hgSilv@users.noreply.github.com>
2021-08-10 12:51:52 -0500

    Create TestRunner.java
    Needed just to check if CI is working

--- src/test/java/cucumberOptions/TestRunner.java
+++ src/test/java/cucumberOptions/TestRunner.java
@@ -6,7 +6,8 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 
 //@RunWith(Cucumber.class)
 @CucumberOptions(
-		features = "src/test/java/features/",
+		//features = "src/test/java/features/",
+		features = {"src/test/java/features/login.feature", "src/test/java/features/logout.feature"},
 		glue = "stepDefinitions"
 		)
 
